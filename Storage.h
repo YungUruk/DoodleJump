b2e3c46bc6527438864155ea1754ba5d657001be
YungUruk <shemberov.vova@ro.ru>
2019-02-10 20:01:33 +0300

    game project

---
+++ Storage.h
@@ -0,0 +1,29 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+#include <string>
+#include <sqlite3.h>
+#include <time.h>
+#include <vector>
+
+
+struct record_db
+{
+	std::string name;
+	int value;
+};
+
+class Storage
+{
+private:
+	std::string name;
+	sqlite3 *db;
+public:
+	Storage();
+	Storage(std::string name, std::string database);
+	record_db get_last_record();
+	std::vector<record_db> get_records(int count);
+	void add_record(int value);
+	~Storage();
+};
+
+#endif // STORAGE_H
